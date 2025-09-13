63b3e0e00d7903628bac502cb6098658f4c4766f
arogya <arogyabichpuria16@gmail.com>
2025-09-13 16:19:43 +0530

    begin with vector spaces

---
+++ numpy/main.py
@@ -0,0 +1,15 @@
+# tutorial to start learning about numpy
+
+import numpy as np
+
+# create a numpy array
+arr = np.array([1, 2, 3, 4, 5])
+
+# execute square root on the array and print the result
+print(np.sqrt(arr))
+
+# print the array
+print(arr)
+
+# print the shape of the array
+
